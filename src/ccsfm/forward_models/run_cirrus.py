c1e3e3827f4702bc3099e6507e8d4c58c497c143
Lars Petter Hauge <larsenhauge@gmail.com>
2024-09-18 09:33:24 +0200

    Add forward model validation

--- src/ccsfm/forward_models/run_cirrus.py
+++ src/ccsfm/forward_models/run_cirrus.py
@@ -1,17 +1,22 @@
 from typing import Optional
+import subprocess
 
 from ert import (
     ForwardModelStepDocumentation,
     ForwardModelStepPlugin,
+    ForwardModelStepJSON,
+    ForwardModelStepValidationError,
 )
 
 
 class Cirrus(ForwardModelStepPlugin):
+    EXECUTABLE = "/prog/pflotran/bin/_runcirrus"
+
     def __init__(self) -> None:
         super().__init__(
             name="CIRRUS",
             command=[
-                "/prog/pflotran/bin/_runcirrus",
+                self.EXECUTABLE,
                 "-q",
                 "local",
                 "-n",
@@ -23,6 +28,19 @@ class Cirrus(ForwardModelStepPlugin):
             default_mapping={"<NUM_CPU>": 1, "<VERSION>": "latest"},
         )
 
+    def validate_pre_experiment(self, fm_step_json: ForwardModelStepJSON) -> None:
+        version_idx = fm_step_json["argList"].index("-v") + 1
+        return_value = subprocess.run(
+            [self.EXECUTABLE, "--print-available-versions"],
+            capture_output=True,
+        )
+
+        if (requested_version := fm_step_json["argList"][version_idx]) not in (
+            available_versions := str(return_value.stdout)
+        ):
+            raise ForwardModelStepValidationError(
+                f"Requested version: {requested_version}, is not available. Must be one of {available_versions}"
+            )
 
     @staticmethod
     def documentation() -> Optional[ForwardModelStepDocumentation]:
