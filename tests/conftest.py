6e332c8e06b72608540f4d5a1570d4ab03a28309
Lars Petter Hauge <larsenhauge@gmail.com>
2024-09-20 13:02:02 +0200

    Add skip when cirrus is not available

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,24 @@ import pytest
 def setup_tmpdir(tmpdir):
     with tmpdir.as_cwd():
         yield
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--ert-integration",
+        action="store_true",
+        default=False,
+        help="Run ERT integration tests",
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--ert-integration"):
+        # Do not skip tests when --ert-integration is supplied on pytest command line
+        return
+    skip_ert_integration = pytest.mark.skip(
+        reason="need --ert-integration option to run"
+    )
+    for item in items:
+        if "ert-integration" in item.keywords:
+            item.add_marker(skip_ert_integration)
