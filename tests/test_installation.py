3bcfc0f95e5a339555f029190d3222d69c795cdb
Lars Petter Hauge <larsenhauge@gmail.com>
2025-05-06 14:56:55 +0200

    Validate CIRRUS arguments pre-experiment

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -3,12 +3,14 @@ import pytest
 import subprocess
 
 from ert.plugins import ErtPluginManager
+from ert.config import ErtConfig
+from ert.config.parsing.config_errors import ConfigValidationError
 
 from ccsfm.forward_models.run_cirrus import Cirrus
 
 DEFAULT_CONFIG = """
 JOBNAME TEST
-
+NUM_CPU 1
 QUEUE_SYSTEM LOCAL
 QUEUE_OPTION LOCAL MAX_RUNNING 1
 
@@ -23,6 +25,22 @@ FORWARD_MODEL {}({})
 def test_forward_model_installation():
     pm = ErtPluginManager()
     assert Cirrus in pm.forward_model_steps
+
+
+@pytest.mark.parametrize(
+    "call_content,expected_failure",
+    [
+        pytest.param("<NUM_CPU>=1, <VERSION>=1", "It is not allowed to modify"),
+        pytest.param("<VERSION>=1, <INCORRECT>=1", "Missing required arguments"),
+    ],
+)
+def test_parameter_validation(call_content, expected_failure):
+    config = DEFAULT_CONFIG.format("CIRRUS", call_content)
+    with open("config.ert", "w", encoding="utf-8") as file:
+        file.write(config)
+
+    with pytest.raises(ConfigValidationError, match=expected_failure):
+        ErtConfig.with_plugins().from_file("config.ert")
 
 
 @pytest.mark.uses_cirrus
